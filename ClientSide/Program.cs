4445b5b93ca853dce15dd530e07a71cb9d95f0cf
Ahmed_hb29 <Ahmed_hb29@STHQ0118-07>
2024-10-10 11:41:07 +0400

    Add project files.

---
+++ ClientSide/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Client:");
+
+var client = new UdpClient();
+
+var endPoint = new IPEndPoint(IPAddress.Loopback, 27001);
+
+try
+{
+    while (true)
+    {
+        var imagePath = @"C:\Users\Ahmed_hb29\Desktop\images.jpg";
+        using (var readFs = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+        {
+            var bytes = new byte[1024];
+            var len = 0;
+
+
+            while ((len = readFs.Read(bytes, 0, bytes.Length)) > 0)
+            {
+                client.Send(bytes, bytes.Length, endPoint);
+                Console.WriteLine("Image sending...");
+            }
+        }
+    }
+   
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Server:");
+
+
+var server = new UdpClient(27001);
+
+var remoteEndPoit = new IPEndPoint(IPAddress.Any, 0);
+
+
+try
+{
+    while (true)
+    {
+        var bytes = server.Receive(ref remoteEndPoit);
+        byte[] imageb = bytes;
+        var imagePath = @"C:\Users\Ahmed_hb29\Desktop\images.jpg";
+
+        using (var writefs = new FileStream(imagePath, FileMode.CreateNew))
+        {
+            writefs.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
